c4f03e225ff067e241346ab7ba48962390df8bc6
eununsg08 <69507865+eununsg08@users.noreply.github.com>
2020-08-12 11:01:05 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toggleBtn = document.querySelector('navs_toogleButton');
-const menu = document.querySelector('navs_menu');
-const sns = document.querySelector('navs_sns');
+const toggleBtn = document.querySelector('.navs_toogleButton');
+const menu = document.querySelector('.navs_menu');
+const sns = document.querySelector('.navs_logo');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', ()=> {
     menu.classList.toggle('active');
     sns.classList.toggle('active');
 });

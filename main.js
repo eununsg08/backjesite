e42db5033ab6955deabd415dfdbfde3c66d75b21
eununsg08 <69507865+eununsg08@users.noreply.github.com>
2020-10-26 17:28:38 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navs_toogleButton');
-const menu = document.querySelector('.navs_menu');
-const sns = document.querySelector('.navs_logo');
-
-toggleBtn.addEventListener('click', ()=> {
-    menu.classList.toggle('active');
-    sns.classList.toggle('active');
-});

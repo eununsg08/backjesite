36ec815920a1cab4d4a07f906873024d7e79f9e8
eununsg08 <69507865+eununsg08@users.noreply.github.com>
2021-02-21 22:23:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,37 +7,37 @@ toggleBtn.addEventListener('click', ()=> {
     sns.classList.toggle('active');
 });
 
-class UserStorage {
-    loginUser(id, password) {
-        return new Promise((resolve,reject) => {
-                if (
-                    (id === '민경일' && password === 'ㅁㄹㅇㅌ') ||
-                    (id === '민경이' && password === 'ㅁㄹ2ㅌ')
-                ) {
-                    resolve(id);
-                } else {
-                    reject(new Error('not found'));
-                }
+// class UserStorage {
+//     loginUser(id, password) {
+//         return new Promise((resolve,reject) => {
+//                 if (
+//                     (id === '민경일' && password === 'ㅁㄹㅇㅌ') ||
+//                     (id === '민경이' && password === 'ㅁㄹ2ㅌ')
+//                 ) {
+//                     resolve(id);
+//                 } else {
+//                     reject(new Error('not found'));
+//                 }
             
-        });    
-    }        
+//         });    
+//     }        
         
-    getRoles(user) {
-        return new Promise((resolve, reject) => {
-            if (user === 'ellie') {
-                resolve({ name: 'ellie', role: 'admin'});
-            } else {
-                reject(new Error('no access'));
-            }
-        });
-    }
-}
-const userStorage = new UserStorage('ellie');
-const id = prompt('1');
-const password = prompt('2');
+//     getRoles(user) {
+//         return new Promise((resolve, reject) => {
+//             if (user === 'ellie') {
+//                 resolve({ name: 'ellie', role: 'admin'});
+//             } else {
+//                 reject(new Error('no access'));
+//             }
+//         });
+//     }
+// }
+// const userStorage = new UserStorage('ellie');
+// const id = prompt('1');
+// const password = prompt('2');
 
-userStorage
-.loginUser(id,password)
-.then(userStorage.getRoles)
-.then(user => alert (`성공 ${user.name}, ㅁㄹㅇㅌ네  ${user.role} ㅇㅈ`))
-.catch(console.log)
+// userStorage
+// .loginUser(id,password)
+// .then(userStorage.getRoles)
+// .then(user => alert (`성공 ${user.name}, ㅁㄹㅇㅌ네  ${user.role} ㅇㅈ`))
+// .catch(console.log)
